d67cd7e90ecac6e8c07ec85d792d1fb0cfa33b5f
diudiuliu <shengliu0516@gmail.com>
2020-04-15 17:06:46 +0200

    15/4/20 back premi

--- app/src/main/java/com/bcoop/bcoop/ui/prize/PrizeViewModel.java
+++ app/src/main/java/com/bcoop/bcoop/ui/prize/PrizeViewModel.java
@@ -1,19 +1,10 @@
 package com.bcoop.bcoop.ui.prize;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class PrizeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
     public PrizeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is prize fragment");
-    }
 
-    public LiveData<String> getText() {
-        return mText;
     }
 }
