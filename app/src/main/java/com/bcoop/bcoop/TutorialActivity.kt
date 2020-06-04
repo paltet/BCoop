5e542d42ccd47b858079c8e67d3571a1587d9fdc
xavierespinos <xavieresfe@gmail.com>
2020-06-04 18:37:31 +0200

    Tutorial + bugs

--- app/src/main/java/com/bcoop/bcoop/TutorialActivity.kt
+++ app/src/main/java/com/bcoop/bcoop/TutorialActivity.kt
@@ -1,6 +1,7 @@
 package com.bcoop.bcoop
 
 import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +9,8 @@ import com.github.appintro.AppIntro
 import com.github.appintro.AppIntro2
 import com.github.appintro.AppIntroFragment
 import com.github.appintro.AppIntroPageTransformerType
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class TutorialActivity : AppIntro2() {
@@ -18,6 +21,9 @@ class TutorialActivity : AppIntro2() {
         // Call addSlide passing your Fragments.
         // You can use AppIntroFragment to use a pre-built fragment
 
+
+
+
         setTransformer(AppIntroPageTransformerType.Parallax(
                 titleParallaxFactor = 1.0,
                 imageParallaxFactor = -1.0,
@@ -27,47 +33,47 @@ class TutorialActivity : AppIntro2() {
         addSlide(AppIntroFragment.newInstance(
                 title = getString(R.string.tutorial1),
                 description = getString(R.string.tutorial1_2),
-                backgroundDrawable = R.drawable.foto1
+                backgroundDrawable = getPhoto(1)
 
                 ))
 
         addSlide(AppIntroFragment.newInstance(
                 title = getString(R.string.tutorial3),
                 description = getString(R.string.tutorial3_2),
-                backgroundDrawable = R.drawable.foto3
+                backgroundDrawable = getPhoto(3)
         ))
 
         addSlide(AppIntroFragment.newInstance(
                 title = getString(R.string.tutorial4),
                 //description = getString(R.string.tutorial4_2),
-                backgroundDrawable = R.drawable.foto4
+                backgroundDrawable = getPhoto(4)
 
         ))
         addSlide(AppIntroFragment.newInstance(
                 title = getString(R.string.tutorial5),
                 //description = getString(R.string.tutorial5_2),
-                backgroundDrawable = R.drawable.foto5
+                backgroundDrawable = getPhoto(5)
         ))
         addSlide(AppIntroFragment.newInstance(
                 //title = getString(R.string.tutorial6),
                 //description = getString(R.string.tutorial6_2),
-                backgroundDrawable = R.drawable.foto6
+                backgroundDrawable = getPhoto(6)
         ))
         addSlide(AppIntroFragment.newInstance(
                 //title = getString(R.string.tutorial7),
                 description = getString(R.string.tutorial7_2),
-                backgroundDrawable = R.drawable.foto7
+                backgroundDrawable = getPhoto(7)
 
         ))
         addSlide(AppIntroFragment.newInstance(
                 //title = getString(R.string.tutorial8),
                 description = getString(R.string.tutorial8_2),
-                backgroundDrawable = R.drawable.foto9
+                backgroundDrawable = getPhoto(9)
         ))
         addSlide(AppIntroFragment.newInstance(
                 title = getString(R.string.tutorial9),
                 //description = getString(R.string.tutorial9_2),
-                backgroundDrawable = R.drawable.foto8
+                backgroundDrawable = getPhoto(8)
         ))
 
 
@@ -90,4 +96,74 @@ class TutorialActivity : AppIntro2() {
         startActivity(intent)
 
     }
+
+    private fun getPhoto(num: Int): Int {
+        val lang = getResources().getText(R.string.language)
+        var photo = 0
+
+        when(num){
+            1 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto1
+                    "cast" -> photo = R.drawable.foto1_cast
+                    "eng" -> photo = R.drawable.foto1_eng
+                }
+            }
+            3-> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto3
+                    "cast" -> photo = R.drawable.foto3_cast
+                    "eng" -> photo = R.drawable.foto3_eng
+                }
+            }
+            4 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto4
+                    "cast" -> photo = R.drawable.foto4_cast
+                    "eng" -> photo = R.drawable.foto4_eng
+                }
+
+            }
+            5 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto5
+                    "cast" -> photo = R.drawable.foto5_cast
+                    "eng" -> photo = R.drawable.foto5_eng
+                }
+            }
+            6 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto6
+                    "cast" -> photo = R.drawable.foto6_cast
+                    "eng" -> photo = R.drawable.foto6_eng
+                }
+            }
+            7 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto7
+                    "cast" -> photo = R.drawable.foto7_cast
+                    "eng" -> photo = R.drawable.foto7_eng
+                }
+            }
+            8 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto8
+                    "cast" -> photo = R.drawable.foto8_cast
+                    "eng" -> photo = R.drawable.foto8_eng
+                }
+            }
+            9 -> {
+                when(lang){
+                    "cat" -> photo = R.drawable.foto9
+                    "cast" -> photo = R.drawable.foto9_cast
+                    "eng" -> photo = R.drawable.foto9_eng
+                }
+            }
+
+        }
+
+        return photo
+    }
+
+
 }
--- app/src/main/java/com/bcoop/bcoop/ui/profile/HabilitatsConfiguration.java
+++ app/src/main/java/com/bcoop/bcoop/ui/profile/HabilitatsConfiguration.java
@@ -121,9 +121,9 @@ public class HabilitatsConfiguration extends AppCompatActivity {
                 public void onClick(View view) {
                     boolean newState = !list.get(position).isChecked();
                     list.get(position).checked = newState;
-                    Toast.makeText(getApplicationContext(),
-                            itemStr + "setOnClickListener\nchecked: " + newState,
-                            Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(),
+                      //      itemStr + "setOnClickListener\nchecked: " + newState,
+                        //    Toast.LENGTH_LONG).show();
                 }
             });
 
@@ -199,9 +199,9 @@ public class HabilitatsConfiguration extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                Toast.makeText(HabilitatsConfiguration.this,
-                        ((Item) (parent.getItemAtPosition(position))).ItemString,
-                        Toast.LENGTH_LONG).show();
+                //Toast.makeText(HabilitatsConfiguration.this,
+                  //      ((Item) (parent.getItemAtPosition(position))).ItemString,
+                    //    Toast.LENGTH_LONG).show();
             }
         });
 
@@ -255,7 +255,7 @@ public class HabilitatsConfiguration extends AppCompatActivity {
 
                     }
                 }
-                Toast.makeText(HabilitatsConfiguration.this, str, Toast.LENGTH_LONG).show();
+                //Toast.makeText(HabilitatsConfiguration.this, str, Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(HabilitatsConfiguration.this, ConfigProfileActivity.class);
                 startActivity(intent);
             }
